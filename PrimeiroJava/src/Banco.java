d8f670eb2f0b7ec018af6eaf94871fb17951bab3
mms-11 <140762703+mms-11@users.noreply.github.com>
2024-02-26 19:14:58 -0300

    mudando banco

--- PrimeiroJava/src/Banco.java
+++ PrimeiroJava/src/Banco.java
@@ -1,67 +1,75 @@
 public class Banco {
-    private RepositorioContas contas;
-    private int indice;
+    private RepositorioContas repositorio;
+   
     private double taxa = 0.01;
 
     public Banco(RepositorioContas repositorio){//construtor
 
-        this.contas = repositorio; //banco tem 10 contas
-        indice = 0;
+        this.repositorio = repositorio; //banco tem 10 contas
+        
     }
 
-    public void cadastrarConta(Conta c){
-        contas[indice] = c;
-        indice++;
-    }
-
-    public Conta acharConta(String numero){
-        int i;
-        boolean achou = false;
-        Conta encontrada = null;
-
-        for(i = 0; i < contas.length && achou == false; i++){
-            if(contas[i].getNumero().equals(numero)){
-                achou = true;
-                encontrada = contas[i];
-                
-            }
+    public void cadastrarConta(ContaAbstrata c){
+        if (!repositorio.existe(c.getNumero())) {
+            repositorio.inserir(c);
+        } else {
+            System.out.println("Conta já existe.");
         }
-        return encontrada;
+    }
+
+    public ContaAbstrata acharConta(String numero){
+       return repositorio.procurar(numero);
     }
     public void debitarConta(String n,double valor){
-        Conta c;
-        c = this.acharConta(n);
+       ContaAbstrata c = repositorio.procurar(n);
+       if(c != null){
         c.debitar(valor);
+        repositorio.atualizar(c);
+       }else{
+        System.out.println("conta nao encontrada");
+       }
     }
 
     public void creditarConta(String n,double valor){
-        Conta c;
-        c = this.acharConta(n);
-        c.creditar(valor);
-    }
-
-    public void transferir(String conta1, String conta2, double valor){
-        Conta c1, c2;
-        c1 = this.acharConta(conta1);
-        c2 = this.acharConta(conta2);
-
-        c1.debitar(valor);
-        c2.creditar(valor);
-    }
-
-    public void renderJuros(String numero){
-        Conta c;
-        c = this.acharConta(numero);
-        if(c instanceof Poupanca){
-            ((Poupanca) c).renderJuros(taxa);
+        ContaAbstrata c = repositorio.procurar(n);
+        if(c != null){
+         c.creditar(valor);(valor);
+         repositorio.atualizar(c);
+        }else{
+         System.out.println("conta nao encontrada");
         }
     }
 
-    public void renderBonus(String numero){
-        Conta c;
-        c = this.acharConta(numero);
-        if(c instanceof ContaEspecial){
+    public void transferir(String contaOrigem, String contaDestino, double valor) {
+        ContaAbstrata origem = repositorio.procurar(contaOrigem);
+        ContaAbstrata destino = repositorio.procurar(contaDestino);
+        if (origem != null && destino != null) {
+            origem.debitar(valor);
+            destino.creditar(valor);
+            repositorio.atualizar(origem);
+            repositorio.atualizar(destino);
+        } else {
+            System.out.println("Uma das contas não foi encontrada.");
+        }
+    }
+  
+    public void renderJuros(String numero) {
+        ContaAbstrata c = repositorio.procurar(numero);
+        if (c instanceof Poupanca) {
+            ((Poupanca) c).renderJuros(taxa);
+            repositorio.atualizar(c);
+        } else {
+            System.out.println("Conta não é uma poupança.");
+        }
+    }
+  
+    public void renderBonus(String numero) {
+        ContaAbstrata c = repositorio.procurar(numero);
+        if (c instanceof ContaEspecial) {
             ((ContaEspecial) c).renderBonus();
+            repositorio.atualizar(c);
+        } else {
+            System.out.println("Conta não é uma conta especial.");
         }
     }
 }
